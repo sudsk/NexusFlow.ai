01d50db1ec4d44e0b13bf30bfdaea4d01fa8c6eb
Suds Kumar <suds123@gmail.com>
2025-03-15 07:16:16 +0000

    Update __init__.py

--- nexusflow/capabilities/__init__.py
+++ nexusflow/capabilities/__init__.py
@@ -8,16 +8,33 @@ This package contains implementations of specific capabilities:
 - Analysis: Data analysis capability
 """
 
-# Import once implemented
-# from nexusflow.capabilities.reasoning import ReasoningCapability
-# from nexusflow.capabilities.retrieval import RetrievalCapability
-# from nexusflow.capabilities.coding import CodingCapability
-# from nexusflow.capabilities.analysis import AnalysisCapability
+from nexusflow.capabilities.reasoning import ReasoningCapability
+from nexusflow.capabilities.retrieval import RetrievalCapability
+from nexusflow.capabilities.coding import CodingCapability
+from nexusflow.capabilities.analysis import AnalysisCapability
+
+# Create instances of capabilities
+reasoning_capability = ReasoningCapability()
+retrieval_capability = RetrievalCapability()
+coding_capability = CodingCapability()
+analysis_capability = AnalysisCapability()
+
+# List of all capabilities
+all_capabilities = [
+    reasoning_capability,
+    retrieval_capability,
+    coding_capability,
+    analysis_capability
+]
 
 __all__ = [
-    # These would be added once implemented
-    # 'ReasoningCapability',
-    # 'RetrievalCapability',
-    # 'CodingCapability',
-    # 'AnalysisCapability',
+    'ReasoningCapability',
+    'RetrievalCapability',
+    'CodingCapability',
+    'AnalysisCapability',
+    'reasoning_capability',
+    'retrieval_capability',
+    'coding_capability',
+    'analysis_capability',
+    'all_capabilities'
 ]
