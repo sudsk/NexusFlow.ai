5173e9faa0fcf28930b0d8510cea046c597fac67
Suds Kumar <suds123@gmail.com>
2025-03-15 06:39:59 +0000

    Update __init__.py

--- nexusflow/tools/__init__.py
+++ nexusflow/tools/__init__.py
@@ -1,1 +1,40 @@
+"""
+Tools for NexusFlow.ai
 
+This package contains tools that agents can use:
+- Registry: Central registry for all tools
+- Web Search: Search the web for information
+- Code Execution: Execute code in a sandbox environment
+- Data Analysis: Analyze data and generate insights
+"""
+
+# Import once implemented
+# from nexusflow.tools.registry import ToolRegistry, tool_registry
+# from nexusflow.tools.web_search import WebSearchTool
+# from nexusflow.tools.code_execution import CodeExecutionTool
+# from nexusflow.tools.data_analysis import DataAnalysisTool
+
+# Create empty tool registry for now
+class ToolRegistry:
+    """Placeholder for the tool registry"""
+    def register_tool(self, *args, **kwargs):
+        pass
+    
+    def get_tool(self, *args, **kwargs):
+        return None
+    
+    def list_tools(self):
+        return []
+
+tool_registry = ToolRegistry()
+
+__all__ = [
+    # Tool registry
+    'ToolRegistry',
+    'tool_registry',
+    
+    # These would be added once implemented
+    # 'WebSearchTool',
+    # 'CodeExecutionTool',
+    # 'DataAnalysisTool',
+]
